f456b80df9af9fa0642fe3cf3bd6b5a70cd16b13
sheina906 <yonatanhan18@gmail.com>
2025-08-16 01:29:09 +0300

    removed cursor, added clear and echo commands

--- sOS.c
+++ sOS.c
@@ -3,14 +3,22 @@
 
 void sOS(void)
 {
+	asm volatile ("outb %0, %1" : : "a"((byte) 0x0A), "Nd" ((unsigned short) 0x3D4));
+	asm volatile ("outb %0, %1" : : "a"((byte) 0x20), "Nd"((unsigned short) 0x3D5));
+
+
 	byte str[STR_MAX_LEN];
-	for (int i = 0; i < 5; i++)
+	while (true)
 	{
 		read(str);
-	}
-	clear();
-	while (1)
-	{
-		read(str);
+
+		if (strcmp(str, "clear"))
+		{
+			clear();
+		}
+		if (strcmppre(str, "echo ",  strlen("echo ")))
+		{
+			printStr(str + strlen("echo "));
+		}
 	}
 }
